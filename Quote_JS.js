4fe95477c11b60c4b5c79bfe3c1337343412bdd6
rdollent <rumbalumba89@gmail.com>
2017-02-05 16:53:26 -0700

    Create Quote_JS.js

---
+++ Quote_JS.js
@@ -0,0 +1,42 @@
+//if document is ready
+$(document).ready(function() {
+
+  
+  //function to get JSON
+  function getQuote() {
+    $.ajax({
+      url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+      success: function(a) {
+        var newQuote = a[0]; //grabs first array
+        if ((newQuote.content + " - " + newQuote.title).length > 140) {
+          getQuote();
+        } else {
+          $(".message").html(newQuote.content);
+          $(".author").html(" - " + newQuote.title);
+          console.log($(".message")[0].textContent); //check to see what you grabbed
+        };
+      },
+      cache: false
+    });
+  };
+
+  //click event to get new quote
+  $(".quote").on("click", function(e) {
+    e.preventDefault();
+    $(".msg").animate({
+      opacity: "0"
+    }, 500).delay(300);
+    getQuote();
+    $(".msg").animate({
+      opacity: "1"
+    }, 700);
+
+  });
+  
+  //click event to tweet quote
+  $(".tweet").on("click", function () {
+    window.open("https://twitter.com/intent/tweet?text=" + $(".message")[0].textContent.trim() + $(".author").text());
+  });
+  
+
+});
